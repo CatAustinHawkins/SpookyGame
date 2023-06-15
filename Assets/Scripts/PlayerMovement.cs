72de536cbc3fbe946bda44d308a1cac82a2e0a21
Cat <angelcah321@hotmail.com>
2023-06-15 19:58:09 +0100

    Revert "Updates"
    This reverts commit f754ad4caa6520c4b3690dd2d3bf0df68ae3659d.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,47 +4,34 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5f; //Controls velocity multiplier
-
-    Rigidbody rb; //Tells script there is a rigidbody, we can use variable rb to reference it in further script
-
-    Animator anim; 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>(); //rb equals the rigidbody on the player
-        anim = GetComponent<Animator>();
-
+        
     }
-
 
     // Update is called once per frame
     void Update()
     {
-        float xMove = Input.GetAxisRaw("Horizontal"); // d key changes value to 1, a key changes value to -1
-        float zMove = Input.GetAxisRaw("Vertical"); // w key changes value to 1, s key changes value to -1
-
-        rb.velocity = new Vector3(xMove, rb.velocity.y, zMove) * speed; // Creates velocity in direction of value equal to keypress (WASD). rb.velocity.y deals with falling + jumping by setting velocity to y. 
-
-        if(Input.GetKey(KeyCode.W)) //moving up
+        if (Input.GetKey(KeyCode.W))
         {
-            anim.Play("AndiWalkingDown");
-            Debug.Log("Test");
+            gameObject.transform.Translate(0, 0, -0.01f); //rotate the object in the x
+
+        }
+        //when the player presses G
+        if (Input.GetKey(KeyCode.A))
+        {
+            gameObject.transform.Translate(-0.01f, 0, 0);
+        }
+        //when the player presses H
+        if (Input.GetKey(KeyCode.S))
+        {
+            gameObject.transform.Translate(0, 0, 0.01f);
         }
 
-        if(Input.GetKey(KeyCode.A))//moving left
+        if(Input.GetKey(KeyCode.D))
         {
-            anim.Play("AndiWalkingLeft");
-        }
-
-        if (Input.GetKey(KeyCode.S)) //moving down
-        {
-            anim.Play("AndiWalkingUp");
-        }
-
-        if (Input.GetKey(KeyCode.D))//moving right
-        {
-            anim.Play("AndiWalkingRight");
+            gameObject.transform.Translate(0.01f, 0, 0);
 
         }
     }
